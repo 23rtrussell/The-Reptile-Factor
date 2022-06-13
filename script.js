823baa8be45618cb2c889cc1ec56f0913de7984d
rtrussell <23rtrussell@redmondschools.org>
2022-06-13 10:50:08 -0700

    A little js

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ pic1.addEventListener('click', function(){
 
 const picContainerElement = document.getElementById('content');
 const firstPicElement = document.getElementById('pic1'); 
+function Image(name, imgPath,){
+    this.name = name;
+    this.imgPath = imgPath;
+    this.clicks = 0;
+    this.views = 0;
+}
